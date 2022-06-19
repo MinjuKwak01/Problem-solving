b4a714fd78d69a59409a8bb04479c4369293eb62
MinjuKwak01 <105683527+MinjuKwak01@users.noreply.github.com>
2022-06-19 22:31:23 +0900

    [Silver V] Title: 수들의 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1789. 수들의 합/수들의 합.cc
+++ 백준/Silver/1789. 수들의 합/수들의 합.cc
@@ -1,22 +1,24 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
- 
-int main(int argc, const char * argv[]) {
-    long long S;
-    int num = 1;
-    int cnt = 0;
-    long long sum = 0;
-    
-    cin >> S;
-    while(1){
-        sum += num;
-        cnt++;
-        if(sum > S){
-            cnt--;
-            break;
-        }
-        num++;
-    }
-    cout << cnt << '\n';
-    return 0;
+
+int main()
+{
+	long long n;
+	cin >> n;
+	long long sum = 0;
+	int i = 1;
+	int cnt = 0;
+	while (1)
+	{
+		sum += i;
+		cnt++;
+		i++;
+		if (sum > n) {
+			cnt--;
+			break;
+		}
+	}
+	cout << cnt;
+	return 0;
+
 }
