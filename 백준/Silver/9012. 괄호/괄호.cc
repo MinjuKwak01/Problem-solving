6c44c58166d09e35ce7c63a15d0e65e41c2285b5
MinjuKwak01 <105683527+MinjuKwak01@users.noreply.github.com>
2022-06-27 21:21:48 +0900

    [Silver IV] Title: 괄호, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.cc
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<stack>
+#include<string>
+using namespace std;
+
+int main()
+{
+
+	int tc;
+	cin >> tc;
+
+	string ps;
+	for (int i = 0; i < tc; i++) {
+		cin >> ps;
+		stack<char> stk;
+		for (int j = 0; j < ps.length(); j++) {
+			if (ps[j] == '(') {
+				stk.push(ps[j]);
+			}
+			else if(ps[j]==')'){
+				if (!stk.empty()&&stk.top()=='(')
+					stk.pop();
+				else {
+					stk.push(ps[j]);
+				}
+			}
+		}
+		if (stk.empty())
+			cout << "YES" << endl;
+		else
+			cout << "NO" << endl;
+	}
+
+	return 0;
+}
