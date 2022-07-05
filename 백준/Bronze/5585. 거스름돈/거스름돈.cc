6014276464b695f7ff0f08339527f02f4305e91a
MinjuKwak01 <105683527+MinjuKwak01@users.noreply.github.com>
2022-07-06 00:48:00 +0900

    [Bronze II] Title: 거스름돈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5585. 거스름돈/거스름돈.cc
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+	int money;
+	cin >> money;
+
+	vector<int> v = {500, 100, 50, 10, 5, 1};
+
+	int left = 1000 - money;
+	int cnt = 0;
+	for (int i = 0; i < 6; i++) {
+		cnt=cnt+left / v[i];
+		left %= v[i];
+	}
+
+	cout << cnt;
+	return 0;
+}
