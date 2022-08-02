0895006d174db22fbdcce88b9c75a88629bead14
MinjuKwak01 <105683527+MinjuKwak01@users.noreply.github.com>
2022-08-02 17:47:07 +0900

    [Silver IV] Title: ATM, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.cc
+++ 백준/Silver/11399. ATM/ATM.cc
@@ -2,30 +2,17 @@
 #include<algorithm>
 using namespace std;
 
+int N, result;
+int P[1002];
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 
-int a[1000];
-int sum[1000];
-int main()
-{
-	int n;
-	cin >> n;
-
-	for (int i = 0; i < n; i++) {
-		cin >> a[i];
-	}
-	sort(a, a + n);
-
-	for (int i = 0; i < n; i++) {
-		if (i == 0)
-			sum[i] = a[i];
-		else {
-			sum[i] = a[i] + sum[i - 1];
-		}
-	}
-	int result = 0;
-	for (int i = 0; i < n; i++) {
-		result += sum[i];
-	}
+	cin >> N;
+	for (int i = 0; i < N; i++) 
+		cin >> P[i];
+	sort(P, P + N);
+	for (int i = 0; i < N; i++)
+		result += P[i] * (N - i);
 	cout << result;
-	return 0;
 }
