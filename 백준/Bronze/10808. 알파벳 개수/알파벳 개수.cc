a0d623bed7fc873d54005de2cdd136418e98e660
MinjuKwak01 <105683527+MinjuKwak01@users.noreply.github.com>
2022-08-21 20:48:33 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -1,18 +1,17 @@
 #include<iostream>
 using namespace std;
 
+int alpha[26];
 int main()
 {
 	string s;
 	cin >> s;
 
-	int a[26] = { 0 };
-	for (int i = 0; i < s.size(); i++) {
-		a[s[i] - 'a']++;
+	for (int i = 0; i < s.length(); i++) {
+		alpha[s[i] - 'a']++;
 	}
 
 	for (int i = 0; i < 26; i++) {
-		cout << a[i] << " ";
+		cout << alpha[i] << ' ';
 	}
-	return 0;
 }
