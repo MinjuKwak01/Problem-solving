9d2f666d7a97fd9dbccfed3b14bb9e0961803040
MinjuKwak01 <105683527+MinjuKwak01@users.noreply.github.com>
2022-09-13 19:04:25 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 44 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define X first
+#define Y second
+
+int n, a, b;
+pair<int, int> p[100004];
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> n;
+  for(int i = 0; i < n; i++) {
+    cin >> p[i].X >> p[i].Y;
+  }
+  sort(p, p + n);
+  for(int i = 0; i < n; i++) cout << p[i].X << ' ' << p[i].Y << '\n';
+}
