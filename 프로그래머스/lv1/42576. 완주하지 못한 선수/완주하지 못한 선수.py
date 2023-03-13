0fe49a8f1d684532b498f5cfdad39abcaf73b7b7
MinjuKwak01 <105683527+MinjuKwak01@users.noreply.github.com>
2023-03-13 10:41:56 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 70.59 ms, Memory: 26.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,10 @@
+def solution(participant, completion):
+
+    completion.sort()
+    participant.sort()
+    
+    for i in range(len(completion)):
+        if participant[i] != completion[i]:
+            return participant[i]
+    
+    return participant[-1]
